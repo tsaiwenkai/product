615e4ac8a3f7a0212d2e84ac4ee6523da7d2ffdf
tsaiwenkai <t816048we@gmail.com>
2022-07-04 22:28:08 +0800

    7/4

--- slnProduct_core/prjProduct_core/Controllers/ShopController.cs
+++ slnProduct_core/prjProduct_core/Controllers/ShopController.cs
@@ -46,5 +46,22 @@ namespace prjProduct_core.Controllers
             pd.Star = q.Star;
             return View(pd);
         }
+        public IActionResult addToCart(int? id)
+        {
+            CProductViewModel pd = new CProductViewModel();
+            var q = db.Products.FirstOrDefault(p => p.ProductId == id);
+            pd.ProductId = q.ProductId;
+            pd.ProductName = q.ProductName;
+            pd.CategoryId = q.CategoryId;
+            var q1 = db.Categories.FirstOrDefault(p => p.CategoryId == q.CategoryId);
+            pd.CategoryName = q1.CategoriesName;
+            pd.Country = q.Country;
+            pd.Price = q.Price;
+            pd.Description = q.Description;
+            pd.Stock = q.Stock;
+            pd.TakeDown = q.TakeDown;
+            pd.Star = q.Star;
+            return View(pd);
+        }
     }
 }
