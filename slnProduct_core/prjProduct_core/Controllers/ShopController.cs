33485ff5d2d1fabe7a1a072355b248151e6d4dbb
tsaiwenkai <t816048we@gmail.com>
2022-07-04 17:31:49 +0800

    7/4

--- slnProduct_core/prjProduct_core/Controllers/ShopController.cs
+++ slnProduct_core/prjProduct_core/Controllers/ShopController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using prjProduct_core.Models;
+using prjProduct_core.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,17 +13,32 @@ namespace prjProduct_core.Controllers
         CoffeeContext db = new CoffeeContext();
         public IActionResult view()
         {
-            var q = db.Products;
+
+            var q = db.Products.Select(p => new CProductViewModel()
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                CategoryId = p.CategoryId,
+                Category = p.Category,
+                Country = p.Country,
+                Price = p.Price,
+                Description = p.Description,
+                Stock = p.Stock,
+                TakeDown = p.TakeDown,
+                Star = p.Star
+            });
             return View(q);
         }
         public IActionResult detail(int? id)
         {
-            Product pd = new Product();
+            CProductViewModel pd = new CProductViewModel();
             var q = db.Products.FirstOrDefault(p => p.ProductId == id);
             pd.ProductId = q.ProductId;
             pd.ProductName = q.ProductName;
-
-            pd.CountryId = q.CountryId;
+            pd.CategoryId = q.CategoryId;
+            var q1 = db.Categories.FirstOrDefault(p => p.CategoryId == q.CategoryId);
+            pd.CategoryName = q1.CategoriesName;
+            pd.Country = q.Country;
             pd.Price = q.Price;
             pd.Description = q.Description;
             pd.Stock = q.Stock;
---
+++ slnProduct_core/prjProduct_core/ViewModel/CProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using prjProduct_core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjProduct_core.ViewModel
+{
+    public class CProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int? CountryId { get; set; }
+        public decimal? Price { get; set; }
+        public string Description { get; set; }
+        public int? Stock { get; set; }
+        public int? ClickCount { get; set; }
+        public bool TakeDown { get; set; }
+        public double? Star { get; set; }
+        public Category Category { get; set; }
+        public  Country Country { get; set; }
+    }
+}
