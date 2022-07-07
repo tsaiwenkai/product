3afe81dba4173f65ef05db5b9f6173edfac5251c
tsaiwenkai <t816048we@gmail.com>
2022-07-07 23:41:37 +0800

    7/7

--- slnProduct_core/prjProduct_core/Controllers/ShopController.cs
+++ slnProduct_core/prjProduct_core/Controllers/ShopController.cs
@@ -17,7 +17,7 @@ namespace prjProduct_core.Controllers
             db = context;
         }
 
- 
+
         public IActionResult view()
         {
 
@@ -36,7 +36,7 @@ namespace prjProduct_core.Controllers
             });
             return View(q);
         }
- 
+
         public IActionResult detail(int? id)
         {
             CProductViewModel pd = new CProductViewModel();
@@ -91,8 +91,8 @@ namespace prjProduct_core.Controllers
 
         public IActionResult partialViewForCatgory(int id)
         {
-            
-            var q = db.Products.Where(p=>p.CategoryId==id).Select(p => new CProductViewModel()
+
+            var q = db.Products.Where(p => p.CategoryId == id).Select(p => new CProductViewModel()
             {
                 ProductId = p.ProductId,
                 ProductName = p.ProductName,
@@ -107,22 +107,110 @@ namespace prjProduct_core.Controllers
             });
             return PartialView(q);
         }
-        public IActionResult LoadContry(int id)
+        public IActionResult partialViewContry(int id)
         {
-            var q = db.Products.Where(p=>p.CountryId==id).Select(p => new CProductViewModel()
+            var q = db.Coffees.Where(p => p.CountryId == id).Select(p => new CCoffeeViewModel()
             {
                 ProductId = p.ProductId,
-                ProductName = p.ProductName,
-                CategoryId = p.CategoryId,
-                Category = p.Category,
+                CoffeeId = p.CoffeeId,
+                CoffeeName = p.CoffeeName,
+                CountryId = p.CountryId,
+                RoastingId = p.RoastingId,
+                ProcessId = p.ProcessId,
+                PackageId = p.PackageId,
+                ConstellationId = p.ConstellationId,
+                RainForest = p.RainForest,
                 Country = p.Country,
-                Price = p.Price,
-                Description = p.Description,
-                Stock = p.Stock,
-                TakeDown = p.TakeDown,
-                Star = p.Star
+                Package = p.Package,
+                Process = p.Process,
+                Product = p.Product,
+                Roasting = p.Roasting
             });
-            return PartialView(q); 
+            return PartialView(q);
+        }
+        public IActionResult partialViewRoast(int id)
+        {
+            var q = db.Coffees.Where(p => p.RoastingId == id).Select(p => new CCoffeeViewModel()
+            {
+                ProductId = p.ProductId,
+                CoffeeId = p.CoffeeId,
+                CoffeeName = p.CoffeeName,
+                CountryId = p.CountryId,
+                RoastingId = p.RoastingId,
+                ProcessId = p.ProcessId,
+                PackageId = p.PackageId,
+                ConstellationId = p.ConstellationId,
+                RainForest = p.RainForest,
+                Country = p.Country,
+                Package = p.Package,
+                Process = p.Process,
+                Product = p.Product,
+                Roasting = p.Roasting
+            });
+            return PartialView(q);
+        }
+        public IActionResult partialViewProcess(int id)
+        {
+            var q = db.Coffees.Where(p => p.ProcessId == id).Select(p => new CCoffeeViewModel()
+            {
+                ProductId = p.ProductId,
+                CoffeeId = p.CoffeeId,
+                CoffeeName = p.CoffeeName,
+                CountryId = p.CountryId,
+                RoastingId = p.RoastingId,
+                ProcessId = p.ProcessId,
+                PackageId = p.PackageId,
+                ConstellationId = p.ConstellationId,
+                RainForest = p.RainForest,
+                Country = p.Country,
+                Package = p.Package,
+                Process = p.Process,
+                Product = p.Product,
+                Roasting = p.Roasting
+            });
+            return PartialView(q);
+        }
+        public IActionResult partialViewPacking(int id)
+        {
+            var q = db.Coffees.Where(p => p.PackageId == id).Select(p => new CCoffeeViewModel()
+            {
+                ProductId = p.ProductId,
+                CoffeeId = p.CoffeeId,
+                CoffeeName = p.CoffeeName,
+                CountryId = p.CountryId,
+                RoastingId = p.RoastingId,
+                ProcessId = p.ProcessId,
+                PackageId = p.PackageId,
+                ConstellationId = p.ConstellationId,
+                RainForest = p.RainForest,
+                Country = p.Country,
+                Package = p.Package,
+                Process = p.Process,
+                Product = p.Product,
+                Roasting = p.Roasting
+            });
+            return PartialView(q);
+        }
+        public IActionResult partialViewContinent(int id)
+        {
+            var q = db.Coffees.Where(p => p.ConstellationId == id).Select(p => new CCoffeeViewModel()
+            {
+                ProductId = p.ProductId,
+                CoffeeId = p.CoffeeId,
+                CoffeeName = p.CoffeeName,
+                CountryId = p.CountryId,
+                RoastingId = p.RoastingId,
+                ProcessId = p.ProcessId,
+                PackageId = p.PackageId,
+                ConstellationId = p.ConstellationId,
+                RainForest = p.RainForest,
+                Country = p.Country,
+                Package = p.Package,
+                Process = p.Process,
+                Product = p.Product,
+                Roasting = p.Roasting
+            });
+            return PartialView(q);
         }
 
     }
---
+++ slnProduct_core/prjProduct_core/ViewModel/CCoffeeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using prjProduct_core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjProduct_core.ViewModel
+{
+    public class CCoffeeViewModel
+    {
+        public int CoffeeId { get; set; }
+        public int ProductId { get; set; }
+        public string CoffeeName { get; set; }
+        public int CountryId { get; set; }
+        public int RoastingId { get; set; }
+        public int ProcessId { get; set; }
+        public int PackageId { get; set; }
+        public int? ConstellationId { get; set; }
+        public bool RainForest { get; set; }
+
+        public  Country Country { get; set; }
+        public  Package Package { get; set; }
+        public  Process Process { get; set; }
+        public  Product Product { get; set; }
+        public  Roasting Roasting { get; set; }
+    }
+}
